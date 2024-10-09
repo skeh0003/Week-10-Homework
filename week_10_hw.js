4d76d56c6a058927f78de22d0233b395d23b4336
skeh0003 <skeh0003@student.monash.edu>
2024-10-09 23:22:44 +1100

    added the world map from week8 as well as html and css files

---
+++ week_10_hw.js
@@ -0,0 +1,9 @@
+var vg_1 = "./week 8 hw.json";
+vegaEmbed("#worldmap", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "./week_10_homework.json";
+vegaEmbed("#ausmap", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
